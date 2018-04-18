b9cd411b05cc3ecf7d1966c7ccaef51f010949fd
nknhathcmup <38147087+nknhathcmup@users.noreply.github.com>
2018-04-18 13:05:54 +0700

    'introduce-respository'

---
+++ DAL/Implement/NewsResponsibility.cs
@@ -0,0 +1,54 @@
+﻿using DAL.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+using DAL.Context;
+
+namespace DAL.Implement
+{
+    public class NewsResponsibility : INewsResponsibility
+    {
+        private BakeryContext db;
+        public NewsResponsibility(BakeryContext db)
+        {
+            this.db = db;
+        }
+        public void AddNews(News news)
+        {
+            db.news.Add(news);
+            db.SaveChanges();
+        }
+
+        public void DeleteNews(string id)
+        {
+            var news = db.news.Find(id);
+            db.news.Remove(news);
+            db.SaveChanges();
+        }
+
+        public void EditNews(News news)
+        {
+            var newsedit = db.news.SingleOrDefault(p => p.ID == news.ID);
+
+            db.SaveChanges();
+        }
+
+        public News find(string id)
+        {
+            var news = db.news.Find(id);
+            return news;
+        }
+
+        public List<News> getlist(int countnumber = 0)
+        {
+
+            var list = db.news.OrderByDescending(p => p.ID);
+            if (countnumber != 0)
+               return list.Take(countnumber).ToList();
+            return list.ToList();
+        }
+    }
+}
---
+++ DAL/Interface/INewsResponsibility.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interface
+{
+    public interface INewsResponsibility
+    {
+        List<News> getlist(int countnumber = 0);
+        News find(string id);
+        void AddNews(News news);
+        void EditNews(News news);
+
+        void DeleteNews(string id);
+    }
+}
